cde2bb62ea06fc69798c431a725302ce0a0e6ded
tekashi <szymon.programmer@gmail.com>
2024-12-16 20:20:58 +0100

    INF.04-01-26.06-SG Konsola

---
+++ INF.04-01-21.06-SG/ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class SortAlgorithm
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Podaj element [{i}]: ");
+                while (!int.TryParse(Console.ReadLine(), out array[i]))
+                {
+                    Console.WriteLine("Niepoprawna wartość. Podaj liczbę całkowitą:");
+                }
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    int temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+                }
+            }
+
+            Console.WriteLine("\nPosortowana tablica:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Element [{i}] wynosi: {array[i]}");
+            }
+
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+            Console.ReadLine();
+        }
+    }
+}
