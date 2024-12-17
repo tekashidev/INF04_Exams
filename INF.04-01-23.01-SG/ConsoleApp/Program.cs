11c59042fa896de44b1b76e815cd82ebcc248f6d
tekashi <szymon.programmer@gmail.com>
2024-12-17 22:03:08 +0100

    Euklides

---
+++ INF.04-01-23.01-SG/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        int A = int.Parse(a);
+        int B = int.Parse(b);
+
+        if (A != B)
+        {
+            if (A > B)
+            {
+                A = A - B;
+            }
+            else
+            {
+                B = B - A;
+            }
+            
+            Console.WriteLine(A);
+        }
+        
+    }
+}
