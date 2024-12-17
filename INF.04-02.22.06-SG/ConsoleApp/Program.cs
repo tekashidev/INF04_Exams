d4c58d2fdf9d17b9009a9ef6d8a80521b76e2b65
tekashi <szymon.programmer@gmail.com>
2024-12-17 21:55:01 +0100

    Init

---
+++ INF.04-02.22.06-SG/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Osoba osoba1 = new Osoba();
+        Osoba osoba2 = new Osoba(1, "Jan");
+        Osoba osoba3 = new Osoba(osoba2); // Konstruktor kopiujący
+
+        // Wyświetlanie danych obiektów
+        osoba1.Wyswietl();
+        osoba2.Wyswietl();
+        osoba3.Wyswietl();
+
+    }
+}
