fc28d04a19d42175f12b2a8ed531f9a6da10f6ad
tekashi <szymon.programmer@gmail.com>
2025-01-06 13:00:54 +0100

    KalkulatorWIekowy
    Zadanie utworzone przez AI

---
+++ INF.04-KalkulatorWiekowy/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Year = 2025;
+int Month = 01;
+int Day = 06;
+int CalcBirth = 0;
+bool loop = true;
+
+string TypeYear;
+string TypeMonth;
+string TypeDay;
+
+Console.WriteLine("Podaj Rok");
+TypeYear = Console.ReadLine();
+Console.WriteLine("Podaj Miesiąc");
+TypeMonth = Console.ReadLine();
+Console.WriteLine("Podaj Dzień");
+TypeDay = Console.ReadLine();
+
+int ConvertYear = Convert.ToInt32(TypeYear);
+int ConvertMonth = Convert.ToInt32(TypeMonth);
+int ConvertDay = Convert.ToInt32(TypeDay);
+
+while (loop)
+{
+    CalcBirth = Year - ConvertYear;
+    if (Month < ConvertMonth)
+    {
+        CalcBirth--;
+    }
+    if (Day < ConvertDay)
+    {
+        Console.WriteLine(CalcBirth);
+        loop = false;
+    }
+    else
+    {
+        Console.WriteLine(CalcBirth);
+        loop = false;
+        
+    }
+}
